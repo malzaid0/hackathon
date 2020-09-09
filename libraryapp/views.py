7e01ec197dd3ce92cf0fada01735be2bf996198c
Mohammed Alzaid <m.al-zaid@live.com>
2020-09-10 02:41:44 +0300

    done steps 4-5

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm, SigninForm, BookForm, GenreForm
 from django.contrib.auth import login, authenticate, logout
-from .models import Book,Genre
+from .models import Book, Genre
+from django.db.models import Q
 
 
 # Create your views here.
@@ -21,10 +22,25 @@ def create_book(request):
 
 
 def book_list(request):
+    books = Book.objects.all()
+    query = request.GET.get('search_term')
+    if query:
+        try:
+            books = books.filter(
+                Q(name__icontains=query) |
+                Q(isbn__exact=int(query)) |
+                Q(genre__name__icontains=query)
+            ).distinct()
+        except:
+            books = books.filter(
+                Q(name__icontains=query) |
+                Q(genre__name__icontains=query)
+            ).distinct()
     context = {
-        "books": Book.objects.all()
+        "books": books
     }
     return render(request, 'book_list.html', context)
+
 
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
@@ -32,6 +48,16 @@ def book_detail(request, book_id):
         "book": book,
     }
     return render(request, 'book_detail.html', context)
+
+
+def book_search(request):
+    if request.method == "POST":
+        search_term = request.POST.get("searchTerm")
+        books = Book.objects.filter(name__contains=search_term)
+        context = {
+            "search_result": books,
+        }
+        return render(request, "search_result.html", context)
 
 
 def signup(request):
